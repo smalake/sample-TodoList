5e6ab2b556d07adf0219306da7be9faf513a2478
smalake <koizumim1002@gmail.com>
2022-12-08 13:14:46 +0900

    Create TodoList

---
+++ src/components/InputForm.jsx
@@ -0,0 +1,39 @@
+import {useState} from 'react';
+
+export const InputForm = ({taskList, setTaskList}) => {
+
+    const [inputText, setInputText] = useState('')
+
+    // onSubmitで実行される。「e」は実行時に渡される引数
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // タスクを追加する
+        setTaskList([
+            ...taskList,
+            {
+                id: taskList.length,
+                text: inputText,
+                completed: false
+            }
+        ]);
+        // 入力した文字を消す
+        setInputText('');
+    }
+
+    // onChangeを受け取る。「e」は実行時に渡される引数(onChangeだから、変化した値)
+    const handleChange = (e) => {
+        setInputText(e.target.value);
+    }
+
+    return (
+        <div className='inputForm'>
+            <form onSubmit={handleSubmit}>
+                <input type='text' onChange={handleChange} value={inputText} />
+                <button>
+                    <i className="fas fa-plus"></i>
+                </button>
+            </form>
+        </div>
+    )
+}
